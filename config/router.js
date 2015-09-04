fc023418548cb5ec1ced22f572491432e52aa4d2
zmdelima@up.edu.ph <zmdelima@up.edu.ph>
2015-09-04 12:37:00 +0800

    cmsc100week3

--- config/router.js
+++ config/router.js
@@ -2,13 +2,19 @@ var student = require('./../controllers/student');
 var teacher = require('./../controllers/teacher');
 
 module.exports = function(router) {
-router.route('/students')
-  .get(student.find);
-  
-router.route('/teacher')
-  .get(teacher.find)
-  .post(teacher.create)
-  .put(teacher.update)
-  .delete(teacher.delete);
+	router.route('/students')
+	  .get(student.find)
+	  .post(student.insert);
+	
+	router.route('/students/:id')//expecting parameter to store in id
+		.get(student.findOne)
+	 	.put(student.update)
+	 	.delete(student.remove);
+	 	
+	router.route('/teacher')
+	  .get(teacher.find)
+	  .post(teacher.create)
+	  .put(teacher.update)
+	  .delete(teacher.delete);
 return router;
 };
--- controllers/student.js
+++ controllers/student.js
@@ -1,3 +1,45 @@
-exports.find=function(req,res){
-res.send('Hello World!');
-};
+var db = require(__dirname + '/../lib/mysql');
+
+exports.find=function(req,res,next){
+	db.query('SELECT * FROM student',
+	function(err,rows){
+		if(err) return next(err);
+		res.send(rows);	
+	});
+}
+
+
+exports.findOne = function(req,res,next){
+	db.query('SELECT * FROM student where studNo=?',[req.params.id],
+	function(err,rows){
+		if(err) return next(err);
+		if(rows.length==0)
+			res.status(404).send('Student not Found!');
+		else res.send(rows);
+	});
+}
+
+exports.insert = function(req,res,next){
+	db.query('INSERT INTO student VALUES(?,?,?)',[req.body.studNo, req.body.name, req.body.bdate],
+	function(err,rows){
+		if(err) return next(err);
+		else res.send(rows);
+	});
+}
+
+exports.update = function(req,res,next){
+	db.query('UPDATE student SET ? WHERE studNo=?',[req.body, req.params.id],
+	function(err,rows){
+		if(err) return next(err);
+		if(rows.length==0)res.status(404).send('Student not Found!');
+		res.send(rows);
+	});
+}
+
+exports.remove = function(req,res,next){
+	db.query('DELETE FROM student WHERE studNo=?',[req.params.id],
+	function(err,rows){
+		if(err) return next(err);
+		res.send(rows);
+	});
+}
