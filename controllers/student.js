3337161779ca039f00bbf11d4a240a856a4c8d4a
zmdelima <zmdelima@up.edu.ph>
2015-09-11 12:54:24 +0800

    test,insert,and connection for student database week4

--- controllers/student.js
+++ controllers/student.js
@@ -1,6 +1,7 @@
 var db = require(__dirname + '/../lib/mysql');
 
 exports.find=function(req,res,next){
+	console.log(req.ip+" find()");
 	db.query('SELECT * FROM student',
 	function(err,rows){
 		if(err) return next(err);
@@ -10,6 +11,7 @@ exports.find=function(req,res,next){
 
 
 exports.findOne = function(req,res,next){
+console.log(req.ip+" findOne()");
 	db.query('SELECT * FROM student where studNo=?',[req.params.id],
 	function(err,rows){
 		if(err) return next(err);
@@ -20,6 +22,7 @@ exports.findOne = function(req,res,next){
 }
 
 exports.insert = function(req,res,next){
+  console.log(req.ip+" Insert()");
 	db.query('INSERT INTO student VALUES(?,?,?)',[req.body.studNo, req.body.name, req.body.bdate],
 	function(err,rows){
 		if(err) return next(err);
@@ -28,6 +31,7 @@ exports.insert = function(req,res,next){
 }
 
 exports.update = function(req,res,next){
+  console.log(req.ip+" Update()");
 	db.query('UPDATE student SET ? WHERE studNo=?',[req.body, req.params.id],
 	function(err,rows){
 		if(err) return next(err);
@@ -37,6 +41,7 @@ exports.update = function(req,res,next){
 }
 
 exports.remove = function(req,res,next){
+  console.log(req.ip+" Delete()");
 	db.query('DELETE FROM student WHERE studNo=?',[req.params.id],
 	function(err,rows){
 		if(err) return next(err);
