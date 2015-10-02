9c6bef045ffeace64563618500c9bbe6047add8c
zmdelima <zmdelima@up.edu.ph>
2015-10-02 11:34:32 +0800

    index.html was added w/ new public folder

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ var server = app.listen(5000,function (){
 app.use(require('body-parser')());
   app.use(require('method-override')());
   app.use(require(__dirname +'/config/router')(express.Router()));
+
+
+app.use(express.static(__dirname + '/public'));
 /*
   app.<method>(<path>,<callback>)
   
